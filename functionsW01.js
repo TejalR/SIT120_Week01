c6e7473d762ce7632ebbd4261c4ea8494de2be7d
TejalR <62227603+TejalR@users.noreply.github.com>
2022-08-16 19:50:20 +1000

    Add files via upload

---
+++ functionsW01.js
@@ -0,0 +1,11 @@
+function colourChange(element, color) {
+  element.style.color = color;
+}
+
+function submitAlert() {
+  alert("Submit successful!");
+}
+
+// var datetime = new Date();
+// console.log(datetime);
+// document.getElementById("time").textContent = datetime;
